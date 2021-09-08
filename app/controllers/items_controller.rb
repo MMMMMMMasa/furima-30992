09d8463a4afe373004e319b819b6fc4d7b241015
MMMMMMMasa <75110304+MMMMMMMasa@users.noreply.github.com>
2021-09-08 12:08:54 +0900

    商品情報編集機能＋

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,8 +28,11 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to item_path
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
