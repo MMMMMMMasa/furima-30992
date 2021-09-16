8e795f13194d9cb0ea0eed42456d28234276e6e3
MMMMMMMasa <75110304+MMMMMMMasa@users.noreply.github.com>
2021-09-16 19:40:36 +0900

    商品購入機能完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:index, :create]
+  before_action :prevent_url, only: [:index, :create]
   def index
     @order = Order.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
     if @order.valid?
       pay_item
       @order.save
+      redirect_to root_path
     else
       render :index
     end
-
   end
 
   private
 
   def order_params
     params.require(:order).permit(:postal_code, :shipping_area_id, :city, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
+  def prevent_url
+    if @item.user_id == current_user.id || @item.purchase_record != nil
+      redirect_to root_path
+    end
   end
 
   def pay_item
